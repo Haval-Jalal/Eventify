2261c0bf42368647253bb38bea3ac261cf9cf3ae
Haval-Jalal <Haval.jalal@hotmail.com>
2026-02-01 20:18:38 +0100

    added text for connection string in Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@ namespace Eventify
 {
     internal class Program
     {
+        //Kopplat mot databasen genom att ladda ner "dotnet add package Microsoft.EntityFrameworkCore.SqlServer" och "dotnet add package Microsoft.EntityFrameworkCore.Tools"
+        //Sedan köra "dotnet ef dbcontext scaffold "Server=(localdb)\MSSQLLocalDB;Database=Eventify;Trusted_Connection=True;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer -o Models -f"
+        //i terminalen för att skapa modeller från databasen
+
+
+
+
         static void Main(string[] args)
         {
             MenuService.Run();
